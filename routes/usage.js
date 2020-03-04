3e384bad92baf4d4cd54e7603d52910145ef9641
computer0101 <deveshpatel7643@gmail.com>
2020-03-04 17:05:47 +0530

    implement usage route to get usage of projects

--- routes/usage.js
+++ routes/usage.js
@@ -1,9 +1,61 @@
 const router = require('express').Router();
 
-router.get('/', (req, res) => {
-  return res.status(200).json({
-    msg: 'get: project/usage',
-  });
+const User = require('../models/user');
+const auth = require('../middleware/auth');
+const { getUsageSchema } = require('../validators/usage');
+
+router.get('/', auth, (req, res) => {
+  const result = getUsageSchema.validate({ name: req.query.name });
+  if (result.error) {
+    return res.status(403).json({
+      error: true,
+      errorType: result.error.details[0].path[0],
+      errorMessage: result.error.details[0].message,
+    });
+  }
+
+  User.findOne({ uid: req.user.uid })
+    .then((dbUser) => {
+      if (!dbUser) {
+        return res.status(400).json({
+          error: true,
+          errorType: 'user',
+          errorMessage: 'User does not exist.',
+        });
+      }
+
+      let requests;
+      for (let i = 0; i < dbUser.projects.length; i++) {
+        if (dbUser.projects[i].name === req.query.name) {
+          requests = dbUser.projects[i].requests;
+          break;
+        }
+      }
+
+      if (!requests) {
+        return res.status(400).json({
+          error: true,
+          errorType: 'project',
+          errorMessage: `Project with ${req.query.name} name not found.`,
+        });
+      }
+
+      // TODO: Fetch requests from redis and append it to the requests array.
+
+      return res.status(200).json({
+        error: false,
+        results: {
+          requests,
+        },
+      });
+    })
+    .catch((err) => {
+      return res.status(500).json({
+        error: true,
+        errorType: 'server',
+        errorMessage: err,
+      });
+    });
 });
 
 module.exports = router;
