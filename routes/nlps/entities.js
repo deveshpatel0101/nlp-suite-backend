64ed2bb7b4ff617b862af04777231e61ced99b18
computer0101 <deveshpatel7643@gmail.com>
2020-06-09 20:55:26 +0530

    update nlp routes to fetch output from python backend

--- routes/nlps/entities.js
+++ routes/nlps/entities.js
@@ -1,13 +1,32 @@
 const router = require('express').Router();
+const fetch = require('node-fetch');
 
 const { pushToDb } = require('../../controllers/rateLimit');
 const nlpAuth = require('../../middleware/nlpAuth');
 
 router.post('/', nlpAuth, (req, res) => {
-  pushToDb(req.user.uid, req.user.pid, 'entities', [new Date().getTime()]);
-  return res.status(200).json({
-    msg: 'post: nlp/entities',
-  });
+  fetch(`${process.env.PYTHON_BACKEND_DOMAIN}/entities`, {
+    method: 'post',
+    body: JSON.stringify({ input: req.body.input }),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      pushToDb(req.user.uid, req.user.pid, 'entities', [new Date().getTime()]);
+      return res.status(200).json({
+        output: data.results,
+      });
+    })
+    .catch((err) => {
+      return res.status(400).json({
+        error: true,
+        errorMessage: err,
+      });
+    });
 });
 
 module.exports = router;
